e8fbbf8751cb7c408aea2e20418f8f5ebca85e3d
Marcos Alonso Perez <marcos.alonso@amaris.com>
2021-08-20 17:58:24 +0200

    first commit

---
+++ src/main/java/com/example/debuggingreactor/DebuggingReactorApplication.java
@@ -0,0 +1,13 @@
+package com.example.debuggingreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebuggingReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebuggingReactorApplication.class, args);
+	}
+
+}
